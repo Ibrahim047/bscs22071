e9a5bb7c21a4f6b8479e08c1faf2a2d4f58d2f23
Ibrahim047 <130080029+Ibrahim047@users.noreply.github.com>
2024-09-21 10:51:35 +0500

    make dynamic data handling using JSON
    About me can now be handle dynamically using json file and all done with the help of javascript

--- script.js
+++ script.js
@@ -38,3 +38,57 @@ fetch("footer.html")
             document.getElementById("includefooter").innerHTML = data;
         });
 
+
+// Fetch JSON data
+fetch('data.json')
+  .then(response => response.json())
+  .then(data => {
+    // Populate Skills
+    const skillP1 = document.querySelector('.skill-p1');
+    const skillP2 = document.querySelector('.skill-p2');
+    data.skills[0].forEach(skill => {
+      let skillDiv = document.createElement('div');
+      skillDiv.classList.add('skillwrapper');
+      skillDiv.innerHTML = `
+        <i class="ri-arrow-right-down-line"></i>
+        <h4>${skill}</h4>
+      `;
+      skillP1.appendChild(skillDiv);
+    });
+    data.skills[1].forEach(skill => {
+      let skillDiv = document.createElement('div');
+      skillDiv.classList.add('skillwrapper');
+      skillDiv.innerHTML = `
+        <i class="ri-arrow-right-down-line"></i>
+        <h4>${skill}</h4>
+      `;
+      skillP2.appendChild(skillDiv);
+    });
+
+    // Populate Certifications
+    const certContainer = document.getElementById('certificationholder');
+    data.certifications.forEach(cert => {
+      let certDiv = document.createElement('div');
+      certDiv.classList.add('certificateswithdate');
+      certDiv.innerHTML = `
+        <h4>${cert.title}</h4>
+        <h4>(${cert.date})</h4>
+      `;
+      certContainer.appendChild(certDiv);
+    });
+
+    // Populate Education
+    const eduContainer = document.querySelector('#education');
+    data.education.forEach(edu => {
+      let eduDiv = document.createElement('div');
+      eduDiv.classList.add('certificateswithdate');
+      eduDiv.innerHTML = `
+        <h4>${edu.degree}</h4>
+        <h4>(${edu.year})</h4>
+      `;
+      eduContainer.appendChild(eduDiv);
+    });
+  })
+  .catch(error => console.error('Error fetching data:', error));
+
+
